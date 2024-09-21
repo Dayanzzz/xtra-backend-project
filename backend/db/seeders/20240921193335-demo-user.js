418cf927667e055e7b1add55b3854616485eb740
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-09-21 14:04:57 -0700

    phase5done

--- backend/db/seeders/20240921193335-demo-user.js
+++ backend/db/seeders/20240921193335-demo-user.js
@@ -14,17 +14,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'dayan',
+        lastName: 'zakrzewski',
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'ted',
+        lastName: 'zakrzewski',
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'martha',
+        lastName: 'zakrzewski',
       }
     ], { validate: true });
   },
